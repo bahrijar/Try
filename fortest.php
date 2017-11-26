e4659f425af2890d2919945f8a011170fda096ff
siputra12 <siputra0770@gmail.com>
2017-11-26 14:06:11 +0700

    Update fortest.php

--- fortest.php
+++ fortest.php
@@ -1,10 +1,9 @@
 <?php
 
 for($a=0;$a<18662400000;$a++){
-print $a."Work2";
-if($a==10){
-sleep(60);
-
-print"next 1 menit lagi";
+print $a."Work2\n";
+if($a%10==0){
+print "next 1 menit lagi\n";
+  sleep(60);
 }
 }
