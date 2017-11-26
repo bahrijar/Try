9ee69c14005e14f002a4752e255cc223fe10676c
siputra12 <siputra0770@gmail.com>
2017-11-26 13:39:26 +0700

    Update fortest.php

--- fortest.php
+++ fortest.php
@@ -1,8 +1,13 @@
 <?php
+if(!file_exists("start.txt")){
+  $h = fopen($b,"w");
+fwrite($h,"0");
+fclose($h);
+  {
 $b = @file_get_contents("start.txt");
 for($a=$b;$a<11;$a++){
 print $a."Work2";
-if($a==10 OR !file_exists("start.txt")){
+if($a==10){
 sleep(60);
 $h = fopen($b,"w");
 fwrite($h,"0");
