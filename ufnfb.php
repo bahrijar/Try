7596c416a5f229b4fb6b2af80b84127a050f8954
siputra12 <siputra0770@gmail.com>
2017-11-28 14:09:28 +0700

    Update ufnfb.php

--- ufnfb.php
+++ ufnfb.php
@@ -22,7 +22,7 @@ if (!file_exists($username.".ig")) {
 } else {
     echo "Type? (1 = Yang Gak Follback)\nInput : ";
     $type = trim(fgets(STDIN));
-    echo "Jeda Setiap Ekse? (Recom = 1)";
+    echo "Jeda Setiap Ekse? (Recom = 1)\nInput : ";
     $jeda = trim(fgets(STDIN));
     if($type==1) $type = "true";
     $gip    = file_get_contents($username.'.ig');
