066de3b023a2811899c643d184952101dfa017cb
siputra12 <siputra0770@gmail.com>
2017-11-28 22:39:55 +0700

    Create run.php

---
+++ run.php
@@ -0,0 +1,16 @@
+<?php
+echo "List Tools :\n[1]Unfollow Not Followback Instagram\n[2]Like Timeline Instagram\n[3]Bomb Sms Tri\n[4]\nBomb Sms Tsel\nMasukan Pilihanmu : ";
+$pilih = trim(fgets(STDIN));
+if($pilih==1){
+    $type = "unfoll";
+}elseif($pilih==2)
+    $type = "liketl";
+}elseif($pilih==3)
+    $type = "bomb3";
+}elseif($pilih==4)
+    $type = "bombtsel";
+}else{
+    echo "Pilihan Tidak ada, silahkan pilih yang ada!\nMasukan Pilihanmu : ";
+}
+
+require_once($type.".php");
